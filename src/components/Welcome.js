c072dfcc64356e33defc73965787fa24a2cdd913
fang092 <bradleyg36@gmail.com>
2022-10-15 17:07:45 -0400

    playing with welcome bg resps

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,11 @@ import logo from '../images/CprintLogo.svg'
 
 function Welcome() {
   return (
-    <section id="welcome" className='w-full h-[750px] bg-welcome bg-cover '>
+    <section id="welcome" className='w-full h-[750px] xs:h-3/4 sm:h-3/4 md:h-3/4 lg:h-3/4 bg-welcome bg-cover '>
         <div className="w-full h-full bg-black/40 ">
             <div className="flex flex-row w-full h-fit">
                 <nav className='w-full h-14 flex flex-row justify-start items-center p-10'>
-                        <div className='flex justify-start items-center xs:w-1/5sm:w-1/2 md:w-1/2 lg:w-1/2' >
+                        <div className='flex justify-start items-center xs:w-1/5 sm:w-1/2 md:w-1/2 lg:w-1/2' >
                             <a href="#home">
                                 <img src={logo} className=" w-16 h-auto" alt='C-Print Logo'/>
                             </a>
@@ -27,11 +27,11 @@ function Welcome() {
                 
                 
                     <div className='flex flex-col w-1/5 h-fit p-12 space-y-8 xs:w-4/5 sm:w-3/4 md:w-1/2 lg:w-1/2'>
-                        <p className='text-left text-white font-bold  text-2xl sm:text-2xl md:text-4xl lg:text-7xl '>
+                        <p className='text-left text-white font-bold  text-2xl xs:text-2xl sm:text-4xl md:text-6xl lg:text-7xl '>
                         Printing Made Comically Easy.
                         </p>
                         {/* Prompt and SignUp Button */}
-                        <p className='text-left font-latoReg text-white sm:text-sm md:text-xl lg:text-2xl '>
+                        <p className='text-left font-latoReg text-white xs:text-sm sm:text-base md:text-xl lg:text-2xl '>
                             We'll help take the guess-work out of it, so you can focus 
                             on creating it!
                         </p>
