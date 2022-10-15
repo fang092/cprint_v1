6fef2ef20b21506b3e28d69aa5fa51ba9150b25e
fang092 <bradleyg36@gmail.com>
2022-10-15 16:29:26 -0400

    fixed the resp of welcome navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import logo from '../images/CprintLogo.svg'
 
 function Footer() {
   return (
-   <section id="footer" className='flex flex-row mx-auto bg-cprintBlue w-full h-fit p-8 xs:w-full xs:flex-col xs:space-y-8 xs:divide-y xs:divide-dashed sm:w-full sm:flex-col sm:space-y-10 sm:divide-y sm:divide-dashed md:w-full md:flex-row md:divide-none lg:w-full lg:flex-row lg:divide-none'>
+   <section id="footer" className='flex flex-row mx-auto bg-cprintBlue w-full h-fit p-4 xs:w-full xs:flex-col xs:space-y-8 xs:divide-y xs:divide-dashed sm:w-full sm:flex-col sm:space-y-10 sm:divide-y sm:divide-dashed md:w-full md:flex-row md:divide-none lg:w-full lg:flex-row lg:divide-none'>
         <div className=' w-[25%] flex flex-col justify-center items-start xs:w-full xs:items-center sm:w-full sm:justify-center sm:items-center md:w-[25%] md:items-start lg:w-[25%] lg:items-start'>
             <img src={logo} alt="Cprint Logo" />
             <p className="xs:text-base sm:text-base md:text-base lg:text-base  ">
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,18 +5,18 @@ function Welcome() {
   return (
     <section id="welcome" className='w-full h-full bg-welcome bg-cover '>
         <div className="w-full h-full bg-black/40 ">
-            <div className="flex flex-row w-full h-fit ">
+            <div className="flex flex-row w-full h-fit">
                 <nav className='w-full h-14 flex flex-row justify-start items-center p-10  z-40'>
-                        <div className='flex justify-start items-center xs:basis-1/2 sm:basis-1/2 md:basis-2/5 lg:basis-2/4'>
+                        <div className='flex justify-start items-center xs:w-1/2 sm:w-1/2 md:w-1/2 lg:w-1/2' >
                             <a href="#home">
                                 <img src={logo} className=" w-16 h-auto" alt='C-Print Logo'/>
                             </a>
                         </div>
-                        <div className=' flex justify-end items-center space-x-6 xs:basis-1/2 sm:basis-1/2 md:basis-3/5 lg:basis-2/4'>
-                            <button className='CprintBtn text-2xl'>
+                        <div className=' flex justify-end items-center space-x-6 xs:w-1/2   sm:w-1/2 md:w-1/2 lg:w-1/2'>
+                            <button className='CprintBtn text-2xl xs:text-lg xs:w-1/2 sm:w-1/2 md:w-1/4 lg:w-1/6'>
                                 Log In
                             </button>
-                            <a href='#SignUp' className='font-bold text-white text-2xl'>
+                            <a href='#SignUp' className='font-bold  text-white text-2xl xs:text-lg xs:w-1/2 sm:w-1/2 md:w-1/4 lg:w-1/6'>
                                 Sign Up
                             </a>
                         </div>
